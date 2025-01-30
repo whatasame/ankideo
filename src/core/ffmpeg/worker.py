c659476e26627880b24b40c653b1e45e6c1d6688
whatasame <97666463+whatasame@users.noreply.github.com>
2025-01-30 11:42:30 +0900

    feat: Improve FFmpeg worker process handling and enhance output dialog functionality

--- src/core/ffmpeg/worker.py
+++ src/core/ffmpeg/worker.py
@@ -1,3 +1,4 @@
+import select
 import subprocess
 from typing import List
 
@@ -14,28 +15,37 @@ class FFmpegWorker(QObject):
     def __init__(self, command: FFmpegCommand):
         super().__init__()
         self.command = command
+        self.process = None
         self.is_cancelled = False
 
     def run(self):
-        process = subprocess.Popen(
-            self.command.to_full_command(),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            universal_newlines=True,
-        )
-        while True:
-            if self.is_cancelled:
-                process.terminate()
-                break
-            line = process.stdout.readline()
-            if not line:
-                break
-            self.output_ready.emit(line.strip())
-        process.wait()
-        self.finished.emit()
+        with subprocess.Popen(
+                self.command.to_full_command(),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                universal_newlines=True,
+                bufsize=1
+        ) as self.process:
+            while self.process.poll() is None:
+                ready, _, _ = select.select([self.process.stdout], [], [], 0.1)
+                if ready:
+                    line = self.process.stdout.readline()
+                    if line:
+                        self.output_ready.emit(line.strip())
+
+            if not self.is_cancelled:
+                self.output_ready.emit(f"\n[Ankidia] Task completed. Result saved to {self.command.output_path}")
+                self.finished.emit()
+
+        # TODO: handle errors
 
     def cancel(self):
         self.is_cancelled = True
+        if self.process:
+            self.process.terminate()
+
+        if self.command.output_path.exists():
+            os.remove(self.command.output_path)
 
 
 class FFmpegManager:
@@ -53,7 +63,12 @@ class FFmpegManager:
         self.workers = []
         self.tasks_completed = 0
         self.total_tasks = len(commands)
-        self.output_dialog = WorkerOutputDialog(self.total_tasks, self.dialog_parent)
+
+        self.output_dialog = WorkerOutputDialog(
+            parent=self.dialog_parent,
+            num_tasks=self.total_tasks,
+            on_cancel=self.cancel_all_tasks
+        )
 
     def start_ffmpeg_tasks(self):
         self.tasks_completed = 0
@@ -63,17 +78,18 @@ class FFmpegManager:
 
         for i, command in enumerate(self.commands):
             worker = FFmpegWorker(command)
-            worker.output_ready.connect(lambda text, idx=i: self.output_dialog.append_output(text, idx))
-            worker.finished.connect(lambda idx=i: self.task_finished(idx))
+            worker.output_ready.connect(lambda text, idx=i: self.output_dialog.append_output(idx, text))
+            worker.finished.connect(lambda: self.task_finished())
             self.workers.append(worker)
+
             self.thread_pool.start(worker.run)
 
-    def task_finished(self, index):
-        self.output_dialog.task_completed(index)
-        self.tasks_completed += 1
+    def task_finished(self) -> None:
+        self.tasks_completed += 1  # TODO: race condition?
 
         if self.tasks_completed == self.total_tasks:
             self.on_all_tasks_completed([worker.command.output_path for worker in self.workers])
+            self.output_dialog.close()
 
     def cancel_all_tasks(self):
         for worker in self.workers:
--- src/core/gui/worker_output_dialog.py
+++ src/core/gui/worker_output_dialog.py
@@ -1,27 +1,41 @@
-from aqt import QDialog, QVBoxLayout, QTextEdit, QLabel
+from typing import Optional, Callable
+
+from aqt import Qt, QDialog, QVBoxLayout, QTextEdit, QLabel, QWidget, QPushButton
 
 
 class WorkerOutputDialog(QDialog):
-    def __init__(self, num_tasks, parent=None):
+    def __init__(self, parent: Optional[QWidget], num_tasks: int, on_cancel: Callable[[], None]) -> None:
         super().__init__(parent)
         self.num_tasks = num_tasks
-        self.init_ui()
+        self.on_cancel = on_cancel
 
-    def init_ui(self):
+        # Remove window buttons
+        self.setWindowFlags(Qt.WindowType.WindowTitleHint | Qt.WindowType.CustomizeWindowHint)
+
         layout = QVBoxLayout()
+
         self.output_texts = []
         for i in range(self.num_tasks):
+            layout.addWidget(QLabel(f"Task {i + 1}:"))
+
             text_edit = QTextEdit()
             text_edit.setReadOnly(True)
             text_edit.setMinimumWidth(600)
             text_edit.setMaximumHeight(100)
-            layout.addWidget(QLabel(f"Task {i + 1}:"))
+
             layout.addWidget(text_edit)
+
             self.output_texts.append(text_edit)
+
+        self.interact_button = QPushButton("Cancel")
+        self.interact_button.clicked.connect(self.on_cancel_clicked)
+        layout.addWidget(self.interact_button)
+
         self.setLayout(layout)
 
-    def append_output(self, text, index):
+    def append_output(self, index: int, text: str) -> None:
         self.output_texts[index].append(text)
 
-    def task_completed(self, index):
-        self.output_texts[index].append("\n--- Task Completed ---")
+    def on_cancel_clicked(self):
+        self.on_cancel()
+        self.close()
